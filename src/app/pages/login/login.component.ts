94c163ecb0e3ef6e9d8775cd777e20c6c0a340b6
soumistar <soumistar@soumistar.com>
2024-11-23 16:38:49 +0000

    login and signup template

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+
+  constructor(){
+    
+  }
+  ngOnInit(): void {
+
+  }
+
+  passwordFieldType: string = 'password';
+
+  togglePasswordVisibility() {
+    this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+  }
+}
