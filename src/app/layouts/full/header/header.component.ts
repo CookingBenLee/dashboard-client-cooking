d5f8a9d538c984e8cee7d564e022992b23f68923
soumistar <soumistar@soumistar.com>
2024-11-28 16:26:16 +0000

    logout

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -4,12 +4,16 @@ import {
   EventEmitter,
   Input,
   ViewEncapsulation,
+  OnInit,
 } from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 import { MatButtonModule } from '@angular/material/button';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
+import { TokenService } from 'src/app/services/token/token.service';
 
 @Component({
   selector: 'app-header',
@@ -19,11 +23,33 @@ import { MatButtonModule } from '@angular/material/button';
   encapsulation: ViewEncapsulation.None,
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  user: any
+
   @Input() showToggle = true;
   @Input() toggleChecked = false;
   @Output() toggleMobileNav = new EventEmitter<void>();
   @Output() toggleCollapsed = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(private userService: UserService, private tokenService: TokenService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.user=this.tokenService.getUser()
+    console.log(this.user);
+
+  }
+
+
+  // getCurrentUser(): void {
+  //   this.user = this.tokenService.getUser();
+  //   console.log('Utilisateur connecté :', this.user);
+  // }
+
+
+  logout(): void {
+    console.log("desconnexion");
+
+    this.tokenService.signOut();
+    this.router.navigate(['/login']);
+  }
 }
