063794d635b757675be98852dc66effd7322079f
Ibrahima BALLO <ballo081048@MacBook-Pro-BALLO.local>
2024-12-25 20:40:58 +0000

    update user createc by adress

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -121,24 +121,26 @@ export class SignupComponent implements OnInit{
             console.log("Adresse creeeeeeeeeeeeeeeeeeeeeeeeer");
             this.compteUser.address = response.data
             this.messageService.add({key:'tc', severity: 'success', summary: 'Success', detail: "Adresse creé avec succes"});
+
+            console.log('Données adresse envoyées:', this.adresseS);
+
+            console.log('Données utilisateur envoyées:', this.compteUser);
+            this.userService.createUser(this.compteUser).subscribe(
+              (data: any) => {
+                console.log('Utilisateur créé:', data);
+                this.router.navigate(['/login'], {
+                  queryParams: {
+                    success: `Compte ${this.compteUser.prenom} ${this.utilisateur.nom} créé avec succès !`
+                  }
+                });
+              },
+              (error) => {
+                console.error('Erreur lors de la création de l\'utilisateur:', error);
+              }
+            );
           }
         )
-        console.log('Données adresse envoyées:', this.adresseS);
-
-        console.log('Données utilisateur envoyées:', this.compteUser);
-        this.userService.createUser(this.compteUser).subscribe(
-          (data: any) => {
-            console.log('Utilisateur créé:', data);
-            this.router.navigate(['/login'], {
-              queryParams: {
-                success: `Compte ${this.compteUser.prenom} ${this.utilisateur.nom} créé avec succès !`
-              }
-            });
-          },
-          (error) => {
-            console.error('Erreur lors de la création de l\'utilisateur:', error);
-          }
-        );
+        
       } else {
         this.errorMessage = "Les mots de passe ne correspondent pas.";
       }
