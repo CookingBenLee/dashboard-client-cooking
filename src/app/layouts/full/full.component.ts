020789cbb04607567b8e7a5dfcf91fe6756a68e6
Ibrahima BALLO <ballo081048@MacBook-Pro-BALLO.local>
2025-01-23 14:39:53 +0000

    update version number

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -193,7 +193,7 @@ export class FullComponent implements OnInit {
       link: '/apps/todo',
     },
   ];
-  fixedDateTime: string = 'Numero de version : 2025/01/20_22:00'; 
+  fixedDateTime: string = 'Numero de version : 2025/01/23_14:00'; 
 
   //YYYYMMDD_HH:MM "2025/01/16_18:47"
 
