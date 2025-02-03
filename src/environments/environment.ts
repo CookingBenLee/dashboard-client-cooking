ed85e8c4a583ab09349b0d42c8e47fcb417812db
Ibrahima BALLO <ballo081048@MacBook-Pro-BALLO.local>
2025-02-03 14:24:59 +0000

    corrsct product new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   baseUrl: 'http://localhost:4200',
   production: false,
   apiUrl:"http://92.222.10.20:9999",
-  // apiUrl:"http://localhost:9999",
+  //apiUrl:"http://localhost:9999",
 };
