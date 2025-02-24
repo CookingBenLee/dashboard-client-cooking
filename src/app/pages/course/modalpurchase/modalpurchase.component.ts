14426d45b49135354fefc66aa6eac344e669826d
soumistar <ismailatoure435@gmail.com>
2025-02-24 18:58:52 +0000

    get his product

--- src/app/pages/course/modalpurchase/modalpurchase.component.ts
+++ src/app/pages/course/modalpurchase/modalpurchase.component.ts
@@ -239,7 +239,7 @@ cuuren: any
 
   async getProducts(){
     const user = this.tokenService.getUser();
-    await this.productService.getAllValidated().then(data =>{
+    await this.productService.getAll(user.id).then(data =>{
       console.log(data)
       this.products=data
       //this.productes[0]=this.products[0]
