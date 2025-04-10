fd7b384b958d26f8df36c996c9f80f7cd5e0def3
soumistar <ismailatoure435@gmail.com>
2025-04-10 00:00:26 +0000

    server

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -193,7 +193,7 @@ export class FullComponent implements OnInit {
       link: '/apps/todo',
     },
   ];
-  fixedDateTime: string = 'Numero de version : 2025/04/8_00:09'; 
+  fixedDateTime: string = 'Numero de version : 2025/04/9_00:05'; 
 
   //YYYYMMDD_HH:MM "2025/01/16_18:47"
 
