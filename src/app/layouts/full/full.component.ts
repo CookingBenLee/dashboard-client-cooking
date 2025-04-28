221b1bfe249c00b1b94dac6112231f04322f1a14
Ismaila TOURE <it.toure@orangemali.local>
2025-04-28 16:49:24 +0000

    after update

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -193,7 +193,7 @@ export class FullComponent implements OnInit {
       link: '/apps/todo',
     },
   ];
-  fixedDateTime: string = 'Numero de version : 2025/04/17_00:07'; 
+  fixedDateTime: string = 'Numero de version : 2025/04/28_16:01'; 
 
   //YYYYMMDD_HH:MM "2025/01/16_18:47"
 
