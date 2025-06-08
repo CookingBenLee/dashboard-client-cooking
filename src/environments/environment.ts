b20e9151d6f1c5cfd4393ed0ff1fdf02592a60e7
Oliver <services@olive-services.net>
2025-06-08 23:46:20 +0000

    Correction preparation et details d'un plat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'http://localhost:4200',
   production: false,
-  //apiUrl:"http://92.222.10.20:9999",
-  apiUrl:"http://localhost:9999",
+  apiUrl:"http://92.222.10.20:9999",
+  // apiUrl:"http://localhost:9999",
 };
