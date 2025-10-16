837545c6f0288ff29199454259ba3162f415b4d2
Oliver <services@olive-services.net>
2025-10-16 05:43:51 +0000

    Fix: Configuration pour développement local
    - Changement de l'URL API de 92.222.10.20:5000 vers localhost:5000
    - Configuration pour développement local
    - Résolution du problème de timeout de connexion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   baseUrl: 'http://localhost:4200',
   production: false,
   //---------------------------------------------------//
-  apiUrl:"http://92.222.10.20:5000",
-  // apiUrl:"http://localhost:5000",
+  // apiUrl:"http://92.222.10.20:5000",
+  apiUrl:"http://localhost:5000",
 };
